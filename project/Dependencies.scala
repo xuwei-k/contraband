a75953c38b5824b23ae66b41b2cd0bc6aa5283f6
xuwei-k <6b656e6a69@gmail.com>
2016-11-28 18:49:26 +0900

    wip Scala 2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,22 +1,21 @@
 import sbt._
 
 object Dependencies {
-  val sjsonnewCore = "com.eed3si9n" %% "sjson-new-core" % "0.4.2"
+  val sjsonnewCore = "com.eed3si9n" %% "sjson-new-core" % "0.5.0"
 
   private val jsonTuples = Seq(
-    ("org.json4s", "json4s-core", "3.2.10"),
-    ("org.spire-math", "jawn-parser", "0.6.0"),
-    ("org.spire-math", "json4s-support", "0.6.0")
+    ("org.spire-math", "jawn-parser", "0.10.4"),
+    ("org.spire-math", "jawn-json4s", "0.10.4")
   )
 
   val jsonDependencies = jsonTuples map {
     case (group, mod, version) => (group %% mod % version).exclude("org.scala-lang", "scalap")
   }
 
-  val scalaCheckVersion = "1.11.5"
+  val scalaCheckVersion = "1.13.4"
   val junitInterface    = "com.novocode" % "junit-interface" % "0.11"
   val scalaCheck        = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
-  val scalaTest         = "org.scalatest" %% "scalatest" % "3.0.0"
+  val scalaTest         = "org.scalatest" %% "scalatest" % "3.0.1"
   val parboiled         = "org.parboiled" %% "parboiled" % "2.1.3"
   val diffutils         = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0"
 }
