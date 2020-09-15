5ae041949941b9773decbeeb799e97be37c9e14c
xuwei-k <6b656e6a69@gmail.com>
2020-09-15 05:02:04 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val sjsonNewScalaJson = "com.eed3si9n" %% "sjson-new-scalajson" % "0.9.0"
   val scalaTest         = "org.scalatest" %% "scalatest" % "3.2.2"
-  val parboiled         = "org.parboiled" %% "parboiled" % "2.1.8"
+  val parboiled         = "org.parboiled" %% "parboiled" % "2.2.0"
   val diffutils         = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0"
   val verify            = "com.eed3si9n.verify" %% "verify" % "0.2.0"
 }
