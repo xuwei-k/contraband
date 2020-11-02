60e26971ffe6efa2c0625d9c5fd6657e8cc7fea5
xuwei-k <6b656e6a69@gmail.com>
2020-11-02 06:43:35 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,9 @@ object Dependencies {
   val scala212 = "2.12.12"
   val scala213 = "2.13.3"
 
-  val sjsonNewScalaJson = "com.eed3si9n" %% "sjson-new-scalajson" % "0.9.0"
+  val sjsonNewScalaJson = "com.eed3si9n" %% "sjson-new-scalajson" % "0.9.1"
   val scalaTest         = "org.scalatest" %% "scalatest" % "3.2.2"
-  val parboiled         = "org.parboiled" %% "parboiled" % "2.1.8"
+  val parboiled         = "org.parboiled" %% "parboiled" % "2.2.1"
   val diffutils         = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0"
   val verify            = "com.eed3si9n.verify" %% "verify" % "0.2.0"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.1")
-addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.6")
-addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.4.0")
+addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.6.1")
+addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.4.1")
 addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.6.3")
